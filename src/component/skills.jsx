157378e91a450eb87c3b39afb257a8b6650d1092
David Antonio Toro Medina <david-toro92@hotmail.com>
2021-11-30 05:55:11 -0500

    Update skills.jsx

--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -11,7 +11,7 @@ export const SkillsProcess = props => {
                 <h1 className='mt-5' style={{marginTop:'10px'}}>{props.languages === 'SPANISH' ? 'SISTEMA OPERATIVO' : 'OS'}</h1>
                 <div className="row mt-3">
                     {SystemOP.map((dato) => (
-                        <ShowItem BoxIconColor={dato.color} icon={dato.icons} title={dato.system}
+                        <ShowItem BoxIconColor={'black'} icon={dato.icons} title={dato.system}
                             value={`${(YearsToNow(dato.year, dato.month, dato.day))} ${(props.languages === 'SPANISH' ? 'AÑOS' : 'YEARS')}`}
                             textValue={props.languages === 'SPANISH' ? dato.level.Spanish : dato.level.English} languages={props.languages}
                             library={[]} textLibrary={props.languages === 'SPANISH' ? 'LIBRERIAS' : 'LIBRARY'} hasLibrary={false}/>)
@@ -20,7 +20,7 @@ export const SkillsProcess = props => {
                 <h1 className='mt-5' style={{marginTop:'10px'}}>SOFTWARE</h1>
                 <div className="row mt-3">
                     {SOFTWARE.map((dato) => (
-                        <ShowItem BoxIconColor={dato.color} icon={dato.icons} title={dato.software}
+                        <ShowItem BoxIconColor={'black'} icon={dato.icons} title={dato.software}
                             value={`${(YearsToNow(dato.year, dato.month, dato.day))} ${(props.languages === 'SPANISH' ? 'AÑOS' : 'YEARS')}`}
                             textValue={''} languages={props.languages}
                             library={[]} textLibrary={props.languages === 'SPANISH' ? 'LIBRERIAS' : 'LIBRARY'} hasLibrary={false}/>)
@@ -29,7 +29,7 @@ export const SkillsProcess = props => {
                 <h1 className='mt-5' style={{marginTop:'10px'}}>{props.languages === 'SPANISH' ? 'LENGUAJE DE PROGRAMACIÓN': 'PROGRAMMING LANGUAGE'}</h1>
                 <div className="row mt-3">
                     {ProgrammingLanguages.map((dato) => (
-                        <ShowItem BoxIconColor={dato.color} icon={dato.icons} title={dato.language}
+                        <ShowItem BoxIconColor={'black'} icon={dato.icons} title={dato.language}
                             value={`${(YearsToNow(dato.year, dato.month, dato.day))} ${(props.languages === 'SPANISH' ? 'AÑOS' : 'YEARS')}`}
                             textValue={props.languages === 'SPANISH' ? dato.level.Spanish : dato.level.English} languages={props.languages}
                             library={dato.library} textLibrary={props.languages === 'SPANISH' ? 'LIBRERIAS' : 'LIBRARY'} hasLibrary={true}/>)
